617cd4daf397985f0fa7831f0d91c69420e18f2e
Keerthi Panneerselvam <keerthi@kiranum.com>
2021-04-18 19:03:53 +0530

    Minor cleanups

--- src/main/java/com/kiranum/fundmanager/Main.java
+++ src/main/java/com/kiranum/fundmanager/Main.java
@@ -1,4 +1,4 @@
-package com.kiranum.fund.ark;
+package com.kiranum.fundmanager;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
