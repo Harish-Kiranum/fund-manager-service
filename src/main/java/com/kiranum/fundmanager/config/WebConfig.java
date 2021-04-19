7105c5fcce098918200b848fde09a6b22d3cd594
Keerthi Panneerselvam <keerthi@kiranum.com>
2021-04-19 22:22:48 +0530

    Enabled cors

--- src/main/java/com/kiranum/fundmanager/config/WebConfig.java
+++ src/main/java/com/kiranum/fundmanager/config/WebConfig.java
@@ -2,12 +2,21 @@ package com.kiranum.fundmanager.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
-@Configuration
-public class WebConfig {
+import static java.util.Arrays.asList;
+import static java.util.Collections.singletonList;
 
+@Configuration
+public class WebConfig extends WebSecurityConfigurerAdapter {
+
+    // -- Enable CORS: start -- //
     @Bean
     public WebMvcConfigurer addCorsMappings() {
         return new WebMvcConfigurer() {
@@ -18,4 +27,22 @@ public class WebConfig {
             }
         };
     }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.cors();
+    }
+
+    @Bean
+    public CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration configuration = new CorsConfiguration();
+        configuration.setAllowedOrigins(singletonList("*"));
+        configuration.setAllowedMethods(asList("HEAD", "GET", "POST", "PUT", "DELETE", "PATCH"));
+        configuration.setAllowedHeaders(asList("Authorization", "Cache-Control", "Content-Type"));
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", configuration);
+        return source;
+    }
+
+    // -- Enable CORS: end -- //
 }
